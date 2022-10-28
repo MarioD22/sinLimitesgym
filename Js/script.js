abf30de79d96c3f5c1c72d98ec43350280227cbe
MARIOD22 <mariodanielcena@gmail.com>
2022-10-28 09:40:16 -0300

    se cargaron @medias y js

---
+++ Js/script.js
@@ -0,0 +1,5 @@
+function wasap(){ 
+   
+window.open("https://api.whatsapp.com/send?phone=5493564627303&text=Hola,%20vi%20un%20anuncio%20en%20Facebook%20y%20quisiera%20conocer%20más%20sobre%20tus%20productos")
+
+}

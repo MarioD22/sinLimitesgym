e8a1ad5355c6f283efe127524e1298983b388019
MARIOD22 <mariodanielcena@gmail.com>
2022-11-01 11:50:40 -0300

    se hicieron cambios en estilos y medias queries

--- Js/script.js
+++ Js/script.js
@@ -1,3 +1,6 @@
+/*funcion del logo wasap para que envie mensaje directo */
+
+
 function wasap(){ 
    
 window.open("https://api.whatsapp.com/send?phone=5493564627303&text=Hola,%20vi%20un%20anuncio%20en%20Facebook%20y%20quisiera%20conocer%20más%20sobre%20tus%20productos")
@@ -16,3 +19,4 @@ function Menu() {
 		list.classList.add("btn-list-abierto");
 	}
 }
+
